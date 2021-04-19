473ccd312d4751bd2c295a2259f1ff9b1957a043
Jane Hu <30421010+dracarystargaryen@users.noreply.github.com>
2021-04-19 11:23:33 -0400

    Update blockindex.c

--- blockindex.c
+++ blockindex.c
@@ -1,9 +1,11 @@
 typedef int hash;
 typedef char *indextransaction;
 
+//Defines the block in hash structure
 typdef struct Block_T {
     hash previous_block;
+    //Hash blocks goes through with the transaction
     hash block;
-    transaction transactions;
+    transaction transactions; 
 }block_t;
 
