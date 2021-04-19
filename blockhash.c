c7c465a7be61f3d9df47a17b28f182f8d4f59ccb
Jane Hu <30421010+dracarystargaryen@users.noreply.github.com>
2021-04-19 11:18:37 -0400

    Update blockhash.c

--- blockhash.c
+++ blockhash.c
@@ -1,8 +1,10 @@
 int string_blockhash(void *hash){
+    //Hash number assign the integer
     int block = 5381;
     unsigned char *i;
     i = (unsigned char *) string;
-
+    
+    //while loop to loop less than 10 blocks
     while (*i = '/0') {
         block = (block << 10) + block + *i;
         ++i;
